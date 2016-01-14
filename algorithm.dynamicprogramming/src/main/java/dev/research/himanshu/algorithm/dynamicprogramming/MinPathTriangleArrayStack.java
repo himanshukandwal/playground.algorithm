5a2bd62f98cbd2d59a54141b3987daac915b779c
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-01-14 01:00:41 -0600

    Playground Sample Exercises.

--- algorithm.dynamicprogramming/src/main/java/dev/research/himanshu/algorithm/dynamicprogramming/MinPathTriangleArrayStack.java
+++ algorithm.dynamicprogramming/src/main/java/dev/research/himanshu/algorithm/dynamicprogramming/MinPathTriangleArrayStack.java
@@ -1,59 +1,85 @@
 package dev.research.himanshu.algorithm.dynamicprogramming;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 /**
- * Given a triangle, find the minimum path sum from top to bottom. 
- * Each step you may move to adjacent numbers on the row below.
- * For example, given the following triangle
+ * Given a triangle, find the minimum path sum from top to bottom. Each step you
+ * may move to adjacent numbers on the row below. For example, given the
+ * following triangle
  * 
- * [
- *      [2],
- *     [3,4],
- *    [6,5,7],
- *   [4,1,8,3]
+ * [ 
+ *      [2], 
+ *     [3,4], 
+ *    [6,5,7], 
+ *   [4,1,8,3] 
  * ]
  *
  * @author Himanshu Kandwal
  *
  */
 public class MinPathTriangleArrayStack {
-	
+
 	private List<List<Integer>> triangle;
-	
-	public MinPathTriangleArrayStack() {}
+
+	public MinPathTriangleArrayStack() {
+	}
 
 	public MinPathTriangleArrayStack(List<List<Integer>> triangle) {
 		this.triangle = triangle;
 	}
-	
+
 	public void setTriangle(List<List<Integer>> triangle) {
 		this.triangle = triangle;
 	}
-	
+
 	public List<List<Integer>> getTriangle() {
 		return triangle;
 	}
-	
+
 	public int minimumTotal() {
-        Iterator<List<Integer>> triangleListIterator = triangle.iterator();
-        
-        int runningIndex = 0;
-        int path = triangleListIterator.next().get(0);
-        
-        for (;triangleListIterator.hasNext();) {
-            List<Integer> list = triangleListIterator.next();
-            
-            if (list.get(runningIndex) > list.get(runningIndex + 1)) {
-                path += list.get(runningIndex + 1);
-                runningIndex ++; 
-            } else {
-                path += list.get(runningIndex);
-            }
-        }
-        
-        return path;
-    }
-	
+		Iterator<List<Integer>> triangleListIterator = triangle.iterator();
+		int topElement = triangleListIterator.next().get(0);
+
+		List<Integer> finalPathCalculationList = new ArrayList<Integer>();
+		finalPathCalculationList.add(topElement);
+
+		List<Integer> pathCalculationList;
+		int lowestMin = topElement;
+
+		for (;triangleListIterator.hasNext();) {
+			List<Integer> dataList = triangleListIterator.next();
+
+			pathCalculationList = new ArrayList<Integer>();
+			Integer runMinima = null;
+			for (int dataIndex = 0; dataIndex < dataList.size(); dataIndex ++) {
+				int left, right, nodeMinimum;
+
+				if (dataIndex + 1 == dataList.size()) {
+					nodeMinimum = left = finalPathCalculationList.get(dataIndex - 1) + dataList.get(dataIndex);
+					pathCalculationList.add(left);
+				}
+				else if (dataIndex == 0) {
+					nodeMinimum = right = finalPathCalculationList.get(dataIndex) + dataList.get(dataIndex);
+					pathCalculationList.add(right);
+				} else {
+					left = finalPathCalculationList.get(dataIndex - 1) + dataList.get(dataIndex);
+					right = finalPathCalculationList.get(dataIndex) + dataList.get(dataIndex);
+					
+					nodeMinimum = Math.min(left, right);
+					pathCalculationList.add(Math.min(left, right));
+				}
+				
+				if (runMinima == null || runMinima > nodeMinimum) {
+					runMinima = nodeMinimum;
+				}
+			}
+			finalPathCalculationList = pathCalculationList;
+			lowestMin = runMinima;
+		}
+
+		return lowestMin;
+	}
+
 }
--- algorithm.dynamicprogramming/src/test/java/dev/research/himanshu/algorithm/dynamicprogramming/MinPathTriangleArrayStackTest.java
+++ algorithm.dynamicprogramming/src/test/java/dev/research/himanshu/algorithm/dynamicprogramming/MinPathTriangleArrayStackTest.java
@@ -1,5 +1,8 @@
 package dev.research.himanshu.algorithm.dynamicprogramming;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.TestCase;
 
 public class MinPathTriangleArrayStackTest extends TestCase {
@@ -8,8 +11,37 @@ public class MinPathTriangleArrayStackTest extends TestCase {
 		super.setUp();
 	}
 
+	@SuppressWarnings("serial")
 	public void testMinimumTotal() {
-		fail("Not yet implemented");
+		MinPathTriangleArrayStack arrayStack = new MinPathTriangleArrayStack();
+		arrayStack.setTriangle(new ArrayList<List<Integer>>() {{
+			add(new ArrayList<Integer>() {{ add(1); }});
+			add(new ArrayList<Integer>() {{ add(2); add(3);}});
+		}});
+		
+		assertEquals(3, arrayStack.minimumTotal());
 	}
 
+	@SuppressWarnings("serial")
+	public void testMinimumTotal2() {
+		MinPathTriangleArrayStack arrayStack = new MinPathTriangleArrayStack();
+		arrayStack.setTriangle(new ArrayList<List<Integer>>() {{
+			add(new ArrayList<Integer>() {{ add(-10); }});
+		}});
+		
+		assertEquals(-10, arrayStack.minimumTotal());
+	}
+	
+	@SuppressWarnings("serial")
+	public void testMinimumTotal3() {
+		MinPathTriangleArrayStack arrayStack = new MinPathTriangleArrayStack();
+		arrayStack.setTriangle(new ArrayList<List<Integer>>() {{
+			add(new ArrayList<Integer>() {{ add(-1); }});
+			add(new ArrayList<Integer>() {{ add(3); add(2); }});
+			add(new ArrayList<Integer>() {{ add(1); add(-2); add(-1); }});
+		}});
+		
+		assertEquals(-1, arrayStack.minimumTotal());
+	}
+	
 }
