08bebc224f648d7291b0b6b1b9b33c3957f64567
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-02-28 23:37:16 -0600

    Fun with Algorithms

---
+++ algorithm.assignments/src/main/java/dev/research/himanshu/algorithm/assignments/lp1/LevelTwoDriver.java
@@ -0,0 +1,160 @@
+package dev.research.himanshu.algorithm.assignments.lp1;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * Driver Class for level 2.
+ * 
+ * @author G31
+ *
+ */
+public class LevelTwoDriver {
+
+	public static void main(String[] args) {
+		Queue<String> commandQueue = new LinkedList<>();
+		
+		int linenum;
+		String cmd;
+		Scanner inputScanner = new Scanner(System.in);
+		
+		/* we can also use '!' to signal end of commands */
+		while (inputScanner.hasNext()) {
+			String val = inputScanner.next();
+		    if(val.equals("!"))
+		        break; 
+		    
+			linenum = Integer.valueOf(val);
+			cmd = inputScanner.next();
+			commandQueue.add(cmd);
+			
+			System.out.println("|" + linenum + "|" + cmd + "|");
+		}
+		
+		inputScanner.close();
+		int loopingCounter = 0;
+		
+		Map<String, NumberNode> metaMap = new HashMap<>();
+		while (!commandQueue.isEmpty()) {
+			String command =  commandQueue.poll();
+			if (loopingCounter ++ < 2) {
+				metaMap.put(command.split("=")[0].trim(), new ComplexNumberNode(command.split("=")[1].trim()));
+				continue;
+			}	
+			
+			String variable = command.split("=")[0];
+			String expression = command.split("=")[1];
+			
+			if (expression.contains("+")) {
+				System.out.println(" output for : " + command);
+				
+				String operandA = expression.split("\\+")[0];
+				String operandB = expression.split("\\+")[1];
+				
+				NumberNode result = ComplexNumberNode.sum(
+						(isVariableDigit(operandA) ? new NumberNode(Integer.valueOf(operandA)) : metaMap.get(operandA)), 
+						(isVariableDigit(operandB) ? new NumberNode(Integer.valueOf(operandB)) : metaMap.get(operandB)));
+				
+				System.out.println( result + " (decimal representation) ");
+				result.printList();
+				
+				metaMap.put(variable, result);
+			} else if (expression.contains("-")) {
+				System.out.println(" output for : " + command);
+				
+				String operandA = expression.split("\\-")[0];
+				String operandB = expression.split("\\-")[1];
+				
+				NumberNode result = ComplexNumberNode.subtract(
+						(isVariableDigit(operandA) ? new NumberNode(Integer.valueOf(operandA)) : metaMap.get(operandA)), 
+						(isVariableDigit(operandB) ? new NumberNode(Integer.valueOf(operandB)) : metaMap.get(operandB)));
+				
+				System.out.println( result + " (decimal representation) ");
+				result.printList();
+				
+				metaMap.put(variable, result);
+			} else if (expression.contains("*")) {
+				System.out.println(" output for : " + command);
+				
+				String operandA = expression.split("\\*")[0];
+				String operandB = expression.split("\\*")[1];
+				
+				NumberNode result = ComplexNumberNode.product(
+						(isVariableDigit(operandA) ? new NumberNode(Integer.valueOf(operandA)) : metaMap.get(operandA)), 
+						(isVariableDigit(operandB) ? new NumberNode(Integer.valueOf(operandB)) : metaMap.get(operandB)));
+				
+				System.out.println( result + " (decimal representation) ");
+				result.printList();
+				
+				metaMap.put(variable, result);
+			} else if (expression.contains("/")) {
+				System.out.println(" output for : " + command);
+				
+				String operandA = expression.split("/")[0];
+				String operandB = expression.split("/")[1];
+				
+				NumberNode result = ComplexNumberNode.divide(
+						(isVariableDigit(operandA) ? new NumberNode(Integer.valueOf(operandA)) : metaMap.get(operandA)), 
+						(isVariableDigit(operandB) ? new NumberNode(Integer.valueOf(operandB)) : metaMap.get(operandB)));
+				
+				System.out.println( result + " (decimal representation) ");
+				result.printList();
+				
+				metaMap.put(variable, result);
+			} else if (expression.contains("^")) {
+				System.out.println(" output for : " + command);
+				
+				String operandA = expression.split("\\^")[0];
+				String operandB = expression.split("\\^")[1];
+				
+				NumberNode result = ComplexNumberNode.power(
+						(isVariableDigit(operandA) ? new NumberNode(Integer.valueOf(operandA)) : metaMap.get(operandA)), 
+						(isVariableDigit(operandB) ? new NumberNode(Integer.valueOf(operandB)) : metaMap.get(operandB)));
+				
+				System.out.println( result + " (decimal representation) ");
+				result.printList();
+				
+				metaMap.put(variable, result);
+			} else if (expression.contains("%")) {
+				System.out.println(" output for : " + command);
+				
+				String operandA = expression.split("\\%")[0];
+				String operandB = expression.split("\\%")[1];
+				
+				NumberNode result = ComplexNumberNode.mod(
+						(isVariableDigit(operandA) ? new NumberNode(Integer.valueOf(operandA)) : metaMap.get(operandA)), 
+						(isVariableDigit(operandB) ? new NumberNode(Integer.valueOf(operandB)) : metaMap.get(operandB)));
+				
+				System.out.println( result + " (decimal representation) ");
+				result.printList();
+				
+				metaMap.put(variable, result);
+			} else if (expression.contains("~")) {
+				System.out.println(" output for : " + command);
+				
+				String operandA = expression.split("\\~")[0];
+				
+				NumberNode result = ComplexNumberNode.squareRoot(
+						(isVariableDigit(operandA) ? new NumberNode(Integer.valueOf(operandA)) : metaMap.get(operandA)));
+				
+				System.out.println( result + " (decimal representation) ");
+				result.printList();
+				
+				metaMap.put(variable, result);
+			}	
+		}
+	}
+	
+	private static boolean isVariableDigit(String operand) {
+		boolean isDigit = true;
+		
+		for (int index = 0; index < operand.length(); index++)
+			isDigit = isDigit && Character.isDigit(operand.charAt(index));
+		
+		return isDigit;
+	}
+
+}
