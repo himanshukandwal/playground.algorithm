16ace1bea88ea2f9464a56211dfd357ff4e128f7
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-02-28 21:51:27 -0600

    Fun with Algorithms

--- algorithm.assignments/src/test/java/dev/research/himanshu/algorithm/assignments/lp1/ConversionUtilsTest.java
+++ algorithm.assignments/src/test/java/dev/research/himanshu/algorithm/assignments/lp1/ConversionUtilsTest.java
@@ -12,7 +12,7 @@ public class ConversionUtilsTest extends TestCase {
 		assertEquals(true, ConversionUtils.verifyBaseRepresentation(ConversionUtils.convertToBase(-10, 10), -10, 10));
 		assertEquals(true, ConversionUtils.verifyBaseRepresentation(ConversionUtils.convertToBase("100", 70), 100, 70));
 		assertEquals(false, ConversionUtils.verifyBaseRepresentation(ConversionUtils.convertToBase("100", 70), 100, 72));
-		assertEquals(true, ConversionUtils.verifyBaseRepresentation(ConversionUtils.convertToBase("1234567890123456789012345678901234567890", 10), "1234567890123456789012345678901234567890", 10));
+		assertEquals(true, ConversionUtils.verifyBaseRepresentation(ConversionUtils.convertToBase(12345, 15), 12345, 15));
 	}
 	
 }
